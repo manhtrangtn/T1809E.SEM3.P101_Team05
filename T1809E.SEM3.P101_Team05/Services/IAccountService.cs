ecba46871b9ecb70d84382fe2daee40240537677
manhtrangtn <manhtrangtn@gmail.com>
2020-04-24 21:36:02 +0700

    restruct project

---
+++ T1809E.SEM3.P101_Team05/Services/IAccountService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1809E.SEM3.P101_Team05.Services
+{
+    interface IAccountService
+    {
+    }
+}
